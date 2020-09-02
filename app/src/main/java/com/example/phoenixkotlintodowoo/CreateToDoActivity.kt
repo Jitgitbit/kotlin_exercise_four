62bc609aec202100f685844b62dc66867e936a56
Jitgitbit <rowowolf@gmail.com>
2020-09-02 15:48:52 +0200

    CreateToDoActivity made

---
+++ app/src/main/java/com/example/phoenixkotlintodowoo/CreateToDoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.phoenixkotlintodowoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateToDoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_to_do)
+    }
+}
