0fa9d921ee328531232cc3df2c1cbb67dc03f7db
Jitgitbit <rowowolf@gmail.com>
2020-09-02 16:26:39 +0200

    title works

--- app/src/main/java/com/example/phoenixkotlintodowoo/CreateToDoActivity.kt
+++ app/src/main/java/com/example/phoenixkotlintodowoo/CreateToDoActivity.kt
@@ -2,10 +2,20 @@ package com.example.phoenixkotlintodowoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_to_do.*
 
 class CreateToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_to_do)
+
+        saveButton.setOnClickListener {
+            var title = ""
+            if (importantCheckBox.isChecked) {
+                title = "❗️ " + titleEditText.text.toString()
+            } else {
+                title = titleEditText.text.toString()
+            }
+        }
     }
 }
