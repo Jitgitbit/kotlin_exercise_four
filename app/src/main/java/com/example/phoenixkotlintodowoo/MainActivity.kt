5e5e9e48173777a2e1766d57e0bf22493abbe113
Jitgitbit <rowowolf@gmail.com>
2020-09-03 17:59:04 +0200

    changed the colors bis

--- app/src/main/java/com/example/phoenixkotlintodowoo/MainActivity.kt
+++ app/src/main/java/com/example/phoenixkotlintodowoo/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
