28ba258f6985e2a0627350677308554343efe1a7
Jitgitbit <rowowolf@gmail.com>
2020-09-03 14:00:13 +0200

    Just a little trick to avoid typo

--- app/src/main/java/com/example/phoenixkotlintodowoo/CreateToDoActivity.kt
+++ app/src/main/java/com/example/phoenixkotlintodowoo/CreateToDoActivity.kt
@@ -18,8 +18,8 @@ class CreateToDoActivity : AppCompatActivity() {
                 title = titleEditText.text.toString()
             }
 
-            var prefs = getSharedPreferences("com.example.phoenixkotlintodowoo.sharedprefs", Context.MODE_PRIVATE)
-            var todos = prefs.getStringSet("todostrings",setOf())?.toMutableSet()
+            var prefs = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
+            var todos = prefs.getStringSet(getString(R.string.TODO_STRINGS),setOf())?.toMutableSet()
             todos?.add(title)
 
             prefs.edit().putStringSet("todostrings",todos).apply()
