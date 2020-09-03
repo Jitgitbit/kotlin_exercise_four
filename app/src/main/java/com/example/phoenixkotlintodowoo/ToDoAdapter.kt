b9577d04981685102882345f2c05fe60b239ea7d
Jitgitbit <rowowolf@gmail.com>
2020-09-03 14:40:08 +0200

    recyclerviewAdapter identified

--- app/src/main/java/com/example/phoenixkotlintodowoo/ToDoAdapter.kt
+++ app/src/main/java/com/example/phoenixkotlintodowoo/ToDoAdapter.kt
@@ -4,7 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.todo_row.view.*
 
-class ToDoAdapter(val todos: List<String>): RecyclerView.Adapter {
+class ToDoAdapter(val todos: List<String>): RecyclerView.Adapter<ToDoAdapter.ToDoHolder>() {
 
     class ToDoHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
 
